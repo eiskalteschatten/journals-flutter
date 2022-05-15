e3ff8a8f7e51e2e8db4006ea844609da19720b29
Alex Seifert <alex@alexseifert.com>
2022-05-15 20:56:12 +0200

    Update the theme color and add a dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Journals',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.yellow,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          surface: Colors.white,
+          onSurface: Colors.black,
+          brightness: Brightness.light,
+          primary: Colors.black,
+          secondary: Colors.yellow,
+          onSecondary: Colors.black,
+        ),
       ),
+      darkTheme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromRGBO(18, 18, 18, 1),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          surface: Colors.black,
+          onSurface: Colors.white,
+          brightness: Brightness.dark,
+          primary: Colors.black,
+          secondary: Colors.yellow,
+          onSecondary: Colors.black,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Journals'),
     );
   }
